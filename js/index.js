45ec12be38bad85e81c8138fa6adcbd275cc2fa2
farid95g <f95guluzade@gmail.com>
2020-09-19 16:43:41 +0400

    sidebar

---
+++ js/index.js
@@ -0,0 +1,16 @@
+document.querySelectorAll("nav.aside-menu a").forEach(link => {
+    link.onclick = (e) => {
+        e.preventDefault();
+        document.querySelector("nav.aside-menu .active-link").classList.remove("active-link");
+        e.target.classList.add("active-link");
+    }
+})
+
+document.querySelectorAll("aside footer .theme-toggler a").forEach(toggler => {
+    toggler.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector("aside footer .theme-toggler .active-toggler").classList.remove("active-toggler");
+        e.target.parentNode.classList.add("active-toggler");
+        console.log(e.target);
+    })
+})
