e0145a90cb6dafdcfb6401c99d56c83652548d4c
farid95g <f95guluzade@gmail.com>
2020-09-19 23:52:26 +0400

    dark and light modes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 document.querySelectorAll("nav.aside-menu a").forEach(link => {
     link.onclick = (e) => {
-        e.preventDefault();
         document.querySelector("nav.aside-menu .active-link").classList.remove("active-link");
         e.target.classList.add("active-link");
     }
@@ -10,10 +9,36 @@ document.querySelectorAll("aside footer .theme-toggler a").forEach(toggler => {
     toggler.addEventListener("click", (e) => {
         e.preventDefault();
         document.querySelector("aside footer .theme-toggler .active-toggler").classList.remove("active-toggler");
-        e.target.parentNode.classList.add("active-toggler");
-        console.log(e.target);
+        toggler.classList.add("active-toggler");
+        if (toggler.classList.contains("dark-mode")) {
+            document.querySelector("body").classList.add("dark-mode");
+            changeBackground(document.querySelectorAll(".white-bg"), "white-bg", "dark-bg");
+            changeImage(document.querySelectorAll(".log-out"), "assets/icons/log-out-white.svg");
+            if (document.querySelectorAll("td img[src*=svg]") !== null) {
+                changeImage(document.querySelectorAll("td img[src*=svg]"), "assets/icons/ellipsis-white.svg");
+            }
+        } else {
+            document.querySelector("body").classList.remove("dark-mode");
+            changeBackground(document.querySelectorAll(".dark-bg"), "dark-bg", "white-bg");
+            changeImage(document.querySelectorAll(".log-out"), "assets/icons/log-out.svg");
+            if (document.querySelectorAll("td img[src*=svg]") !== null) {
+                changeImage(document.querySelectorAll("td img[src*=svg]"), "assets/icons/ellipsis.svg");
+            }
+        }
     })
 })
+
+const changeImage = (arr, src) => {
+    arr.forEach(image => {
+        image.src = src;
+    })
+}
+const changeBackground = (arr, oldColor, newColor) => {
+    arr.forEach(elm => {
+        elm.classList.remove(oldColor);
+        elm.classList.add(newColor);
+    })
+}
 
 document.getElementById("show-appeal-dropdown").addEventListener("click", () => {
     document.querySelector(".appeal-dropdown").classList.toggle("hidden");
